813b5aae048014ef1921ec7e46421db947d9ab85
octoccoper <octocopper@gmail.com>
2021-03-03 09:09:50 +0200

    adding get request and showing response in Post component

--- src/components/NewPost/NewPost.js
+++ src/components/NewPost/NewPost.js
@@ -19,7 +19,7 @@ class NewPost extends Component {
                 <textarea rows="4" value={this.state.content} onChange={(event) => this.setState({content: event.target.value})} />
                 <label>Author</label>
                 <select value={this.state.author} onChange={(event) => this.setState({author: event.target.value})}>
-                    <option value="Max">Max</option>
+                    <option value="Grogo">Grogo</option>
                     <option value="Manu">Manu</option>
                 </select>
                 <button>Add Post</button>
