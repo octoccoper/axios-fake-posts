ba5aa05299ef39594ed411a2058c4a267f05316c
octoccoper <octocopper@gmail.com>
2021-03-03 15:14:59 +0200

    fetching data on click on post

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -1,20 +1,48 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import './FullPost.css';
 
 class FullPost extends Component {
-    render () {
-        let post = <p>Please select a Post!</p>;
-        post = (
-            <div className="FullPost">
-                <h1>Title</h1>
-                <p>Content</p>
-                <div className="Edit">
-                    <button className="Delete">Delete</button>
-                </div>
-            </div>
 
-        );
+    state = {
+        loadedPost: null
+    };
+
+    componentDidUpdate() {
+
+        if(this.props.id) {
+
+            if(!this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)) {
+
+                axios.get('https://jsonplaceholder.typicode.com/posts/' + this.props.id)
+                .then(response => {
+                    this.setState({loadedPost: response.data});
+                    console.log("[FullPost.js] componentDidUpdate()",response);
+                });
+            }
+        }
+    }
+
+    render () {
+        let  post = <p className="Text">Please select a Post!</p>;
+
+        if(this.props.id) {
+            post = <p className="Text">Content is loading, please wait...</p>;
+        }
+
+        if(this.state.loadedPost) {
+            post = (
+                <div className="FullPost">
+                    <h1>{this.state.loadedPost.title}</h1>
+                    <p>{this.state.loadedPost.body}</p>
+                    <div className="Edit">
+                        <button className="Delete">Delete</button>
+                    </div>
+                </div>
+            );
+        }
+
         return post;
     }
 }
--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -9,7 +9,8 @@ import './Blog.css';
 class Blog extends Component {
 
     state = {
-        posts: []
+        posts: [],
+        selectedPostId: null
     }
 
     componentDidMount () {
@@ -30,10 +31,19 @@ class Blog extends Component {
             });
     }
 
+    postClickedHandler = (id) => {
+        console.log("postClickedHandler, id is:", id);
+        this.setState({selectedPostId: id});
+    }
+
     render () {
 
         const posts = this.state.posts.map(post => {
-            return <Post key={post.id} title={post.title} author={post.author}/>
+            return <Post 
+                    key={post.id}   
+                    title={post.title}
+                    author={post.author}
+                    clicked={() => this.postClickedHandler(post.id)}/>
         });
 
         return (
@@ -42,7 +52,7 @@ class Blog extends Component {
                     {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPostId}/>
                 </section>
                 <section>
                     <NewPost />
